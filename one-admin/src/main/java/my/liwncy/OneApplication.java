05af84676f9310852eecffb8c61613dfc94c2549
wanchang.li <liwncy@qq.com>
2025-08-30 17:37:50 +0800

    feat: 初始化 one-crawler 项目结构
    - 添加 .editorconfig 和 .gitignore 文件
    - 创建 one-admin 模块并添加启动类
    - 编写 pom.xml 文件，配置项目依赖和插件

---
+++ one-admin/src/main/java/my/liwncy/OneApplication.java
@@ -0,0 +1,20 @@
+package my.liwncy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+
+/**
+ * 启动程序
+ *
+ * @author liwncy
+ */
+@SpringBootApplication
+public class OneApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(OneApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  RuoYi-Vue-Plus启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+}
